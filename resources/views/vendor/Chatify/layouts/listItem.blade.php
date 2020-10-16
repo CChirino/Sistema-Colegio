da7bd125fdecd3bd7e0e0e7113b449880c0c6e89
Christopher Chirino <critijo@gmail.com>
2020-10-15 23:08:46 -0400

    Busqueda chat

--- resources/views/vendor/Chatify/layouts/listItem.blade.php
+++ resources/views/vendor/Chatify/layouts/listItem.blade.php
@@ -74,7 +74,7 @@
         {{-- center side --}}
         <td>
         <p data-id="{{ $type.'_'.$user->id }}">
-            {{ strlen($user->name, $user->apellido) > 12 ? trim(substr($user->name,0,12)).'..' : $user->name }} 
+            {{ strlen($user->name) > 12 ? trim(substr($user->name,0,12)).'..' : $user->name }} 
         </td>
         
     </tr>
